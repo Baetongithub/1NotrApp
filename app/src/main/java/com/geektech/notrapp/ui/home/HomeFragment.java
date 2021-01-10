87f0777f296ceb7b00f2a3ef5dfefd413ba264e7
Baetongithub <toktonbekovbaet@gmail.com>
2021-01-10 20:22:38 +0600

    #1HomeWork_NoteApp_Baet_on_Android2

---
+++ app/src/main/java/com/geektech/notrapp/ui/home/HomeFragment.java
@@ -0,0 +1,48 @@
+package com.geektech.notrapp.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.geektech.notrapp.R;
+
+public class HomeFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.fab).setOnClickListener(v ->
+                openForm());
+
+        setFragmentListener();
+    }
+
+    private void setFragmentListener() {
+        getParentFragmentManager().setFragmentResultListener("rk_form", getViewLifecycleOwner(), (requestKey, result) ->
+                Log.e("Home", "text = " + result.getString("text")));
+    }
+
+    private void openForm() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.formFragment);
+
+    }
+}
